907c7cbba66a1f94e03b993a0d49f792ee8d7c14
Dey1122 <145286230+Dey1122@users.noreply.github.com>
2024-11-08 11:16:43 -0500

    loginDesing

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,8 +1,10 @@
-import { View, Text, Alert, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Alert, TextInput, TouchableOpacity, Image } from 'react-native'
 import React, { useRef, useState } from 'react'
 import { wp, hp } from '@/src/helpers/common'
 import { useAuth } from '@/src/context/authContext'
 import { useRouter } from 'expo-router'
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const login = () => {
     const route = useRouter()
@@ -11,6 +13,7 @@ const login = () => {
 
     const emailRef = useRef<string>("")
     const passwordRef = useRef<string>("")
+    const insets = useSafeAreaInsets();
 
     const handleLogin = async () => {
         if (!emailRef.current || !passwordRef.current) {
@@ -36,12 +39,41 @@ const login = () => {
     }
     return (
     
-        <View className='flex-1 p-6'
-            style={{marginTop: hp(10)}}
+        <View className='flex-1'
+            style={{ paddingTop: insets.top }}
         >
-            <View className='gap-4'>
+            <Image source={{ uri:"https://i.ibb.co/7KJyRRW/pexels-saul-siguenza-355267816-18450877.jpg" }} 
+            style={{width:'100%', height:'65%', alignSelf:'center'}} 
+            resizeMode='cover'/>
+            <View style={{
+                    position: 'absolute',
+                    top: '40%',
+                    left: wp(0),
+                    right: wp(0),
+                    backgroundColor: 'white',
+                    borderRadius: 15,
+                    padding: 20,
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 0 },
+                    shadowOpacity: 0.3,
+                    shadowRadius: 5,
+                    elevation: 8,
+                    height:'100%'
+                }}>
+                <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 }}>
+                    Iniciar Sesión
+                </Text>
+                <Text className='pt-3'>Email</Text>
                 <View
-                className='flex-row'
+                className='flex-row items-center' 
+                style={{ 
+                    flexDirection: 'row', 
+                    alignItems: 'center', 
+                    borderColor: 'gray', 
+                    borderWidth: 1, 
+                    borderRadius: 8, 
+                    backgroundColor: '#EDEDED',
+                    marginVertical: 8 }}
                 >
                 <TextInput
                     autoCapitalize='none'
@@ -49,14 +81,27 @@ const login = () => {
                     emailRef.current = text
                     }}
                     placeholder='Email Adress' 
-                    className='text-neutral-600 flex-1'
+                    className='text-neutral-600 flex-1 border-none'
                     placeholderTextColor={'gray'}
+                    style={{
+                        borderBottomWidth: 1, 
+                        borderBottomColor: 'gray', 
+                        padding: 10}}
                 />
+                <Icon name="email-outline" size={24} color="gray"
+                    className='px-2'/>
                 </View>
-
+                <Text className='pt-3 gap-1'>Contraseña</Text>
                 <View
-                    className=' flex-row'
-                >
+                    className='flex-row items-center' 
+                    style={{flexDirection: 'row', 
+                        alignItems: 'center', 
+                        borderColor: 'gray', 
+                        borderWidth: 1, 
+                        borderRadius: 8, 
+                        backgroundColor: '#EDEDED',
+                        marginVertical: 8}}
+                    >
                     <TextInput
                         autoCapitalize='none'
                         onChangeText={(text) => {
@@ -64,13 +109,17 @@ const login = () => {
                         }}
                         secureTextEntry={true}
                         placeholder='Password' 
-                        className='text-neutral-600 flex-1'
+                        className='text-neutral-600 flex-1 border-none'
                         placeholderTextColor={'gray'}
+                        
+                        style={{borderBottomWidth: 1, 
+                            borderBottomColor: 'gray', 
+                            padding: 10}}
                     />
-                    
+                    <Icon name="lock-outline" size={24} color="gray" className='px-2'/>
                 </View>
 
-                <View className='gap-4' style={{marginTop: hp(2)}}>
+                <View className='flex-row' style={{marginTop: hp(2)}}>
                 {
                     loading ? (
                     <View className='flex-row justify-center'>
@@ -79,25 +128,25 @@ const login = () => {
                     ): (
                     <TouchableOpacity
                         onPress={handleLogin}
-                        style={{height: hp(6)}}
+                        style={{height: hp(7), backgroundColor: '#C48437', borderRadius: 8, width:'100%'}}
                         className='bg-neutral-700 rounded-md justify-center items-center'
                     >
-                        <Text style={{fontSize: hp(2.7)}} className='text-white font-semibold tracking-wider'>Sign In</Text>
+                        <Text style={{fontSize: hp(2.7)}} className='text-white font-semibold tracking-wider'>Iniciar Sesion</Text>
                     </TouchableOpacity>
                     )
                 }
                 </View>
 
                 <View 
-                className='flex-row justify-between'
+                className='flex-row justify-center items-center gap-1'
                 style={{marginTop: hp(3)}}
                 >
-                <Text>Quieres registrarte?</Text>
+                <Text>Aún no tienes una cuenta?</Text>
                 <TouchableOpacity
                     onPress={() => route.navigate("/(auth)/register")}
-                    className='flex-row justify-center'
+                    className='flex-row justify-center items-center'
                 >
-                    <Text style={{color: 'blue'}}>Registrate</Text>
+                    <Text style={{color: 'blue'}}>Registrate Ahora</Text>
                 </TouchableOpacity>
                 </View>
             </View>
